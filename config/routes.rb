0f799290011120362e2d681fa365da8a661b2ee2
sumanp <sumanpuri55@gmail.com>
2021-03-01 01:58:05 +0530

    Added changed root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
+  root 'products#index'
   get :hello_world, to: 'pages#hello_world'
 end
