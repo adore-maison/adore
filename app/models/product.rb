97e0f1506fb635ec7fbec4ed03eb526d63f17581
sumanp <sumanpuri55@gmail.com>
2021-04-09 01:51:38 +0530

    Added discard as a product stage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  enum stage: { basic_info: 0, intial_checks: 1, verifed_market_demand: 2, competitors_analysed: 3, reviews_analysed: 4, profit_first_calculation: 5, contacted_suppliers: 6, ordered_samples: 7, placed_order: 8 }
+  enum stage: { basic_info: 0, intial_checks: 1, verifed_market_demand: 2, competitors_analysed: 3, reviews_analysed: 4, profit_first_calculation: 5, contacted_suppliers: 6, ordered_samples: 7, placed_order: 8, discarded: 9 }
   self.per_page = 7
 
   has_many :selection_rules
