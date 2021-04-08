9e3dd178d01678672ded71b5d507ffa2f30c1b65
sumanp <sumanpuri55@gmail.com>
2021-04-08 17:34:33 +0530

    Update product stages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  enum stage: { basic_info: 0, selection_checks: 1, verify_demand: 2, competitor_analysis: 3, review_analysis: 4 }
+  enum stage: { basic_info: 0, intial_checks: 1, verifed_market_demand: 2, competitors_analysed: 3, reviews_analysed: 4, profit_first_calculation: 5, contacted_suppliers: 6, ordered_samples: 7, placed_order: 8 }
   self.per_page = 7
 
   has_many :selection_rules
