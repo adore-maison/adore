fe76b3e0db1ffbaaf6f3b054f8ef3170bf8e27d9
sumanp <sumanpuri55@gmail.com>
2021-04-10 19:05:10 +0530

    handled nil url error

--- app/services/asin_grabber_service.rb
+++ app/services/asin_grabber_service.rb
@@ -14,7 +14,11 @@ class AsinGrabberService
     page = MetaInspector.new("https://www.amazon.in/dp/#{@asin}")
     if page.response.status == 200
       product[:description] = page.parsed.xpath('//*[@id="productTitle"]').text.strip
-      product[:photo_url] = page.parsed.xpath('//*[@id="landingImage"]').first.values[2]
+      if page.parsed.xpath('//*[@id="landingImage"]').first.nil?
+        product[:photo_url] = ''
+      else
+        product[:photo_url] = page.parsed.xpath('//*[@id="landingImage"]').first.values[2]
+      end
       product[:comp_price] = page.parsed.xpath('//*[@id="priceblock_ourprice"]').text.scan(/[.0-9]/).join().to_d
       product[:comp_rating] = page.parsed.xpath('//*[@id="acrPopover"]/span[1]/a/i[1]/span').text.split.first.to_d
       product[:comp_review_count] = page.parsed.xpath('//*[@id="acrCustomerReviewText"]').text.split.first.to_i
